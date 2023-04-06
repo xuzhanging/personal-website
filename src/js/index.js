442e489bbb8a594704349968aea24f2da7801fe5
xuzhanging <2856602581@qq.com>
2023-04-06 19:13:03 +0800

    add loading animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,24 @@
 "use strict";
-
-const loginSection = document.querySelector("#login-section");
-const btnLogin = document.querySelector(".btn-login");
-
-const events = ["click", "touch"];
-events.forEach((e) => {
-  btnLogin.addEventListener(e, function (e) {
-    e.preventDefault();
-    loginSection.classList.add("hidden");
-  });
+console.log("hello");
+const loading = document.querySelector("#loading");
+const blackMoon = document.querySelector("#black-moon");
+const whiteMoon = document.querySelector("#white-moon");
+blackMoon.addEventListener("animationend", function () {
+  setTimeout(function () {
+    loading.style.animation = "fade 3s linear forwards";
+  }, 2000);
 });
+
+// const loginSection = document.querySelector("#login-section");
+// const btnLogin = document.querySelector(".btn-login");
+
+// const events = ["click"];
+// events.forEach((e) => {
+//   btnLogin.addEventListener(e, function (e) {
+//     // e.preventDefault();
+//     loginSection.classList.add("hidden");
+//   });
+// });
 // btnLogin.addEventListener("click", function (e) {
 //   e.preventDefault();
 //   loginSection.classList.add("hidden");
