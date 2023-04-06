44df78c445a86adb8a997a7c113ee863bee15976
xuzhanging <2856602581@qq.com>
2023-04-07 00:52:05 +0800

    fixed errors

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,30 @@
 "use strict";
-console.log("hello");
+
+/*
+ **************************
+ *****loading aniamtion****
+ **************************
+ */
 const loading = document.querySelector("#loading");
 const blackMoon = document.querySelector("#black-moon");
 const whiteMoon = document.querySelector("#white-moon");
+const loginSection = document.querySelector("#login-section");
+const btnLogin = document.querySelector("#btn-login");
+
 blackMoon.addEventListener("animationend", function () {
   setTimeout(function () {
     loading.style.animation = "fade 3s linear forwards";
-  }, 2000);
+  }, 1000);
 });
-
-// const loginSection = document.querySelector("#login-section");
-// const btnLogin = document.querySelector(".btn-login");
+setTimeout(function () {
+  loading.style.display = "none";
+  loginSection.classList.toggle("hidden");
+}, 10000);
 
 // const events = ["click"];
 // events.forEach((e) => {
 //   btnLogin.addEventListener(e, function (e) {
-//     // e.preventDefault();
+//     e.preventDefault();
 //     loginSection.classList.add("hidden");
 //   });
-// });
-// btnLogin.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   loginSection.classList.add("hidden");
 // });
