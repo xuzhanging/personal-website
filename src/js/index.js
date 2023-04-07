f216911240636044f66e0bc9749bfe6f33e2eeb4
xuzhanging <2856602581@qq.com>
2023-04-07 18:25:03 +0800

    add welcome component

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,24 @@
 "use strict";
 
-/*
- **************************
- *****loading aniamtion****
- **************************
- */
 const loading = document.querySelector("#loading");
 const blackMoon = document.querySelector("#black-moon");
 const whiteMoon = document.querySelector("#white-moon");
 const loginSection = document.querySelector("#login-section");
 const btnLogin = document.querySelector("#btn-login");
+const welcome = document.querySelector("#welcome-section");
 
+// 网页进入动画结束后淡出
 blackMoon.addEventListener("animationend", function () {
   setTimeout(function () {
     loading.style.animation = "fade 3s linear forwards";
   }, 1000);
 });
+
+// 然后欢迎页面浮现
 setTimeout(function () {
   loading.style.display = "none";
-  loginSection.classList.toggle("hidden");
+  welcome.classList.toggle("hidden");
+  welcome.style.animation = "fadein 2s linear forwards";
 }, 10000);
 
 // const events = ["click"];
