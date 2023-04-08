2c8906eade7387654540af0407b2a6a6e191f5e4
xuzhanging <2856602581@qq.com>
2023-04-08 19:06:08 +0800

    fixed loading bugs

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,29 @@
 "use strict";
 
-const loading = document.querySelector("#loading");
+// select loading section elements
+const loading = document.querySelector("#loading-section");
 const blackMoon = document.querySelector("#black-moon");
 const whiteMoon = document.querySelector("#white-moon");
-const loginSection = document.querySelector("#login-section");
-const btnLogin = document.querySelector("#btn-login");
+
+// select login section elements
+// const loginSection = document.querySelector("#login-section");
+// const btnLogin = document.querySelector("#btn-login");
+
+// select welcome section elements
 const welcome = document.querySelector("#welcome-section");
 
-// 网页进入动画结束后淡出
+// loading animation finished, then loading section fade out.
 blackMoon.addEventListener("animationend", function () {
   setTimeout(function () {
-    loading.style.animation = "fade 3s linear forwards";
+    loading.style.animation = "loading-fade-out 3s linear forwards";
   }, 1000);
 });
 
-// 然后欢迎页面浮现
+// then welcome section fade in.
 setTimeout(function () {
   loading.style.display = "none";
   welcome.classList.toggle("hidden");
-  welcome.style.animation = "fadein 2s linear forwards";
+  welcome.style.animation = "welcome-fade-in 2s linear forwards";
 }, 10000);
 
 // const events = ["click"];
